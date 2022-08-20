0276fca0e601b98a86c6940bfff13559a7012619
Samuel Kevin <72141979+samuelkev@users.noreply.github.com>
2022-08-20 02:23:42 -0400

    Update responda_por_mim.py

--- responda_por_mim.py
+++ responda_por_mim.py
@@ -3,42 +3,42 @@ from time import sleep
 
 #converte o arquivo txt em lista
 def converte_em_lista(arquivo, lista):
-    with open(arquivo, 'r', encoding='UTF8') as texto:
+    with open(arquivo, "r", encoding="UTF8") as texto:
         for linha in texto:
-            lista.append(str(linha).replace('\n', ''))
+            lista.append(str(linha).replace("\n", ""))
         return lista
 
 frases = []
-frases = converte_em_lista('ditados.txt', frases)
+frases = converte_em_lista("ditados.txt", frases)
 
 #Início do programa
-jogador = input('Informe seu nome: ')
+jogador = input("Informe seu nome: ")
 sleep(1)
-print(f'É uma honra te conhecer {jogador}')
+print(f"É uma honra te conhecer {jogador}")
 sleep(1)
-print(f'Me chamo Harvey, o seu guru pessoal, pode me perguntar qualquer coisa')
+print(f"Me chamo Harvey, o seu guru pessoal, pode me perguntar qualquer coisa")
 sleep(2)
 jogar = True
 perguntas = []
 while jogar == True:
-    print(f'Quer fazer uma pergunta {jogador}?')
-    print('Sim (1)')
-    print('Não (2)')
+    print(f"Quer fazer uma pergunta {jogador}?")
+    print("Sim (1)")
+    print("Não (2)")
     try:
-        resposta = int(input('Sua resposta: '))
+        resposta = int(input("Sua resposta: "))
         if resposta == 1:
-            print('Ok, então faça sua pergunta')
-            pergunta = input('Sua pergunta: ')
+            print("Ok, então faça sua pergunta")
+            pergunta = input("Sua pergunta: ")
             if pergunta not in perguntas:
                 perguntas.append(pergunta)
-                print('Hummm...')
+                print("Hummm...")
                 for i in range(5):
-                    print('...')
+                    print("...")
                     sleep(1)
                 resposta = frases[random.randint(0, len(frases))]
                 print(resposta)
         elif resposta == 2:
             jogar = False
-            print(f'Ok, nos vemos em breve!')
+            print(f"Ok, nos vemos em breve!")
     except:
-        print('Resposta inválida, tente novamente')
+        print("Resposta inválida, tente novamente")
